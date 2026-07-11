02f3d343fa3720f280d1b68a44f7d5f4cb34d708
Vedant Borawake <vedant.borawake262@gmail.com>
2026-07-11 15:54:41 +0530

    feat: pass socket server into pipeline trigger

--- relay-backend/src/routes/runs.ts
+++ relay-backend/src/routes/runs.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express'
 import { createRun, getRun } from '../controllers/runs.controller';
+import { Server } from 'socket.io';
 
 const router = Router();
+
+let ioInstance: Server
+export function setSocketServer(io: Server){
+    ioInstance = io;
+}
 
 router.post('/', createRun);
 
